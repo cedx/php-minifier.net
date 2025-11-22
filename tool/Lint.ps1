cc328e88d56ffd0601e668329814f2114b366d98
Cédric Belin <cedx@outlook.com>
2025-11-22 23:58:25 +0100

    Update the "Lint" script [skip ci]

--- tool/Lint.ps1
+++ tool/Lint.ps1
@@ -1,4 +1,4 @@
 "Performing the static analysis of source code..."
 Import-Module PSScriptAnalyzer
 Invoke-ScriptAnalyzer $PSScriptRoot -Recurse
-Test-ModuleManifest "$PSScriptRoot/../PhpMinifier.psd1" | Out-Null
+Test-ModuleManifest PhpMinifier.psd1 | Out-Null
