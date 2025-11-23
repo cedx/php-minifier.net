e3dc3dc3876eea2e2e5c69e58bb9038eb4a4d6ea
Cédric Belin <cedx@outlook.com>
2025-11-23 14:20:41 +0100

    Code formatting [skip ci]

--- test/AssertExtensions.cs
+++ test/AssertExtensions.cs
@@ -13,7 +13,7 @@ public static class AssertExtensions {
 		/// <param name="action">The action to be tested and which should not throw an exception.</param>
 		/// <param name="message">The message to include in the error when the action throws an exception of type <typeparamref name="T"/>.</param>
 		/// <exception cref="AssertFailedException">The specified action threw an exception of type <typeparamref name="T"/>.</exception>
-		public void DoesNotThrow<T>(Action action, string message = "") where T : Exception {
+		public void DoesNotThrow<T>(Action action, string message = "") where T: Exception {
 			try { action(); }
 			catch (T exception) { Fail(message.Length > 0 ? message : exception.Message); }
 			catch {}
